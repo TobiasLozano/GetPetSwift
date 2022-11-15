fcd54f2573919baecf71be8fdb5d46bae6535ae6
Tobias Lozano <88689090+TobiasLozano@users.noreply.github.com>
2022-11-15 14:03:50 -0500

    Nuevo main.js con base de datos princpal

--- src/main.js
+++ src/main.js
@@ -4,14 +4,13 @@ horaElement = document.getElementById("hora");
 ahoraElement = document.getElementById("ahora");
 const boton = document.getElementById("boton");
 boton.onclick = () => {
-  fetch("https://sheet.best/api/sheets/85b1faa7-57e6-404f-80b7-7531a679eeef")
+  fetch("https://sheet.best/api/sheets/9cd23ca7-c272-4613-a4bd-d8929ecef293")
     .then((response) => response.json())
     .then((data) => {
       let ultimoDato = data[data.length - 1];
       let longitud = ultimoDato.Longitud;
       let latitud = ultimoDato.Latitud;
       let hora = ultimoDato.Hora;
-      let tiempo = new Date("14/11/2022 7:52:33");
       let ahora = new Date(),
         ahoraFormateada =
           [ahora.getDate(), ahora.getMonth() + 1, ahora.getFullYear()].join(
